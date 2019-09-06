13ba1d31b209589748f46f51cc622a910ec70ee9
Sami Lahtinen <sami.lahtinen@helsinki.fi>
2019-09-06 13:28:02 +0300

    Implemented a very thin subset of the Lichess API

---
+++ src/main/java/chess/connection/LichessApi.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chess.connection;
+
+import java.io.IOException;
+import org.json.*;
+
+public class LichessApi {
+    private HTTPHandler http;
+    private String gameId;
+    
+    public LichessApi(String token) {
+        
+    }
+    
+    public String getAccount() throws IOException {
+        return this.http.get("https://lichess.org/api/account");
+    }
+    
+    public String getEvents() throws IOException {
+        return this.http.get("https://lichess.org/api/stream/event");
+    }
+    
+    public String acceptChallenge(String challengeId) throws IOException {
+        return this.http.post("https://lichess.org/api/challenge/%s/accept".format(challengeId), "");
+    }
+    
+    public String getGamestate() throws IOException {
+        return this.http.get("https://lichess.org/api/bot/game/stream/" + gameId);
+    }
+    
+    public String makeMove(String move) throws IOException {
+        return this.http.post("https://lichess.org/api/bot/game/%s/move/%s".format(gameId, move), "offeringDraw=false");
+    }
+}
