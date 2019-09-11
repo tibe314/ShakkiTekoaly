d13c7b3b8890fe9ea24c83cd1443245c0278cf1c
antlammi <antti.lammi@helsinki.fi>
2019-09-11 15:38:01 +0300

    Chess library imported

--- src/main/java/chess/engine/Engine.java
+++ src/main/java/chess/engine/Engine.java
@@ -1,5 +1,17 @@
 package chess.engine;
+import com.github.bhlangonijr.chesslib.move.*;
+import com.github.bhlangonijr.chesslib.*;
 
 public class Engine {
-    
+    public static void main(String[] args){
+        Board b = new Board();
+        b.doMove(new Move(Square.E2, Square.E4));
+        System.out.println(b.toString());
+        try {
+            MoveList moves = MoveGenerator.generateLegalMoves(b);
+            System.out.println(moves);
+        } catch (Exception e) {
+            System.out.println("MOI");
+        } 
+    }
 }
