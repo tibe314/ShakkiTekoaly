ae91cc84e63245b71fb52600f3783a756ddaaf6f
antlammi <antti.lammi@helsinki.fi>
2019-09-11 16:40:06 +0300

    Main loop that generates and prints random moves in Engine class.

--- src/main/java/chess/engine/Engine.java
+++ src/main/java/chess/engine/Engine.java
@@ -1,17 +1,44 @@
 package chess.engine;
 import com.github.bhlangonijr.chesslib.move.*;
+
+import java.util.Random;
+
 import com.github.bhlangonijr.chesslib.*;
 
 public class Engine {
-    public static void main(String[] args){
-        Board b = new Board();
-        b.doMove(new Move(Square.E2, Square.E4));
-        System.out.println(b.toString());
-        try {
-            MoveList moves = MoveGenerator.generateLegalMoves(b);
-            System.out.println(moves);
-        } catch (Exception e) {
-            System.out.println("MOI");
-        } 
+    private Random random; 
+    private Board b;
+    public Engine(){
+        this.random = new Random();
+        this.b = new Board();
     }
+    public Board getBoard(){
+        return this.b;
+    }
+    public Move getMove() throws Exception{
+        MoveList moves = MoveGenerator.generateLegalMoves(b);
+        System.out.println(moves);
+        Move move = moves.get(random.nextInt(moves.size()));
+        return move;
+    }
+
+    public static void main(String[] args){
+        Engine engine = new Engine();
+        Board b = engine.getBoard();   
+        for (int i=0; i<20; i++){
+            try {
+                Move move = engine.getMove();
+                b.doMove(move);   
+               
+            } catch (Exception e) {
+                System.out.println("Error while generating moves: " + e.getMessage() + " at index " + i );
+            }
+         
+            
+        }   
+        System.out.println(b.toString());
+    
+    }
+
+    
 }
