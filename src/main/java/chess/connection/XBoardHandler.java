cffb79fa3329e34d17e8efec281a9d8f54db6c82
antlammi <antti.lammi@helsinki.fi>
2019-09-20 11:26:54 +0300

    Comments to indicate functionality

--- src/main/java/chess/connection/XBoardHandler.java
+++ src/main/java/chess/connection/XBoardHandler.java
@@ -1,6 +1,7 @@
 package chess.connection;
 import java.util.Scanner;
 import chess.TestBot;
+
 public class XBoardHandler {
     private Scanner scanner;
     private TestBot bot;
@@ -11,6 +12,7 @@ public class XBoardHandler {
         while (true){
             if (scanner.nextLine().startsWith("protover")){
                 System.out.append("feature sigint=0 sigterm=0 reuse=0 myname=\"tiraengine\" done=1\n");
+                //set reuse=1 if bot is able to play multiple games in a session
                 System.out.flush();
             }
             run();
@@ -18,20 +20,32 @@ public class XBoardHandler {
 
     }
 
-    public void run(){
-        while (true){
+    public void run() {
+        while (true) {
             String command = scanner.nextLine();
             switch (command){
                 case "new":
+                //play as white
                 break;
                 
                 case "go":
+                //play as black
                 break;
 
                 default:
                 //assume is a move
+                //update internal gamestate
+                //make next move
                 break;
             }
         }
+    }
+
+    public void handleMove(String move){
+        
+    }
+
+    public String makeMove(){
+        return "";
     }
 }
