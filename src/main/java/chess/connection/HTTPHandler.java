43b0a2d7e54d00bb5be765514e47e0039eee5c82
Jere Mustonen <jere.mustonen@helsinki.fi>
2019-09-20 12:33:45 +0300

    Revert "removed unused class", broke the whole thing This reverts commit 5301408444fb06a8be3d54df358310866f542a6d.

---
+++ src/main/java/chess/connection/HTTPHandler.java
@@ -0,0 +1,23 @@
+package chess.connection;
+
+import java.util.Map;
+import kong.unirest.HttpRequest;
+import kong.unirest.Unirest;
+
+public class HTTPHandler {
+    private Map<String, String> headerFields;
+
+    public HTTPHandler(Map<String, String> headerFields) {
+        this.headerFields = headerFields;
+    }
+
+    public HttpRequest get(String urlString) {
+        return Unirest.get(urlString)
+                .headers(headerFields);
+    }
+    /* POST DATA  NOT APPLIED YET */ 
+    public HttpRequest post(String urlString, String postData) {
+        return Unirest.post(urlString)
+                .headers(headerFields);
+    }
+}
