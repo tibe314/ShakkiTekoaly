ced8615311d94d5a25e6d167cbe66fb333b5e3c3
Jere Mustonen <jere.mustonen@helsinki.fi>
2019-09-20 15:17:29 +0300

    Added 'new HTTP handler class

---
+++ src/main/java/chess/connection/HTTPStream.java
@@ -0,0 +1,82 @@
+package chess.connection;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Iterator;
+import java.util.Map;
+
+public class HTTPStream implements Iterator<String>, Closeable {
+
+    private HttpURLConnection conn;
+    private Iterator<String> iterator;
+
+    public HTTPStream() {
+
+    }
+
+    public HTTPStream get(String urlString) {
+        try {
+            URL url = new URL(urlString);
+            this.conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return this;
+    }
+
+    public HTTPStream post(String urlString, String postData) {
+        try {
+            URL url = new URL(urlString);
+            this.conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("POST");
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return this;
+    }
+
+    public HTTPStream setHeaders(String urlString, Map<String, String> headerFields) {
+        try {
+            headerFields.entrySet().forEach((entry) -> {
+                this.conn.setRequestProperty(entry.getKey(), entry.getValue());
+            });
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return this;
+    }
+
+    public HTTPStream connect() {
+        try {
+            this.conn.connect();
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(
+                            this.conn.getInputStream()));
+            this.iterator = reader.lines().iterator();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return this;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return this.iterator.hasNext();
+    }
+
+    @Override
+    public String next() {
+        return this.iterator.next();
+    }
+
+    @Override
+    public void close() throws IOException {
+        this.conn.disconnect();
+    }
+
+}
